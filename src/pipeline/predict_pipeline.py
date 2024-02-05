3905fcd58df2133653f895f8010e4a30d7b2d2c5
Ransingh Satyajit Ray <satyajit239@gmail.com>
2024-02-05 17:57:40 +0530

    debugging why flask app is not running

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,42 +1,40 @@
 import sys
-import os
 import pandas as pd
 from src.exception import CustomException
 from src.utils import load_object
 
-# 
+
 class PredictPipeline:
     def __init__(self):
         pass
-    
-    
-class CustomData:    
+
     def predict(self,features):
         try:
-            model_path = os.path.join("artifacts","model.pkl")
-            preprocessor_path = os.path.join('artifacts','preprocessor.pkl')
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             print("Before Loading")
-            model = load_object(file_path = model_path)
-            preprocessor = load_object(file_path = preprocessor_path)
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
             print("After Loading")
-            data_scaled = preprocessor.transform(features)
-            preds = model.predict(data_scaled)
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
             return preds
+        
         except Exception as e:
             raise CustomException(e,sys)
 
 
-# The customdata class will be responsible for mapping all the data input we are giving to the html to the backend with this particular value
+
 class CustomData:
-    def __init__(self,
-                 gender:str,
-                 race_ethnicity:int,
-                 parental_level_of_education,
-                 lunch:str,
-                 test_preparation_course:str,
-                 reading_score:int,
-                 writing_score:int):
-        
+    def __init__(  self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
+
         self.gender = gender
 
         self.race_ethnicity = race_ethnicity
@@ -51,10 +49,9 @@ class CustomData:
 
         self.writing_score = writing_score
 
-# all the data we are feeding in the ui are mapped to these values
-def get_data_as_data_frame(self):
-    try:
-        custom_data_input_dict = {
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
                 "gender": [self.gender],
                 "race_ethnicity": [self.race_ethnicity],
                 "parental_level_of_education": [self.parental_level_of_education],
@@ -62,9 +59,9 @@ def get_data_as_data_frame(self):
                 "test_preparation_course": [self.test_preparation_course],
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score],
-        }
+            }
 
-        return pd.DataFrame(custom_data_input_dict)
+            return pd.DataFrame(custom_data_input_dict)
 
-    except Exception as e:
-        raise CustomException(e, sys)     
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -52,7 +52,8 @@ def evaluate_models(X_train,y_train, X_test, y_test, models, param):#
 # function responsible for loading the pickle file
 def load_object(file_path):
     try:
-        with open(file_path, 'rb') as file_obj:
-            return dill.load(file_obj)
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)                    
+        raise CustomException(e, sys)                   
